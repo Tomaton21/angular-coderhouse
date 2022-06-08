54041d31f36a0f249af45296d153742bc75f5ce4
Tomas Gonzalez <gonzalez.tomas1806@gmail.com>
2022-06-07 23:50:00 -0300

    Primer desafio
    Primer desafio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
