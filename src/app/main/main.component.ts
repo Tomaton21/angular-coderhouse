8b7b1ba5e47be4f7965d07fe4699131c6ea3a4af
Tomas Gonzalez <gonzalez.tomas1806@gmail.com>
2022-06-21 23:52:29 -0300

    Formulario reactivo
    Formulario reactivo

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { FormBuilder,Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { Usuario } from '../models/usuario';
+import 'animate.css';
 
 @Component({
   selector: 'app-main',
@@ -6,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
-
+ 
+  usuarioModel: Usuario = new Usuario('','','','','')
+  formulario = this.fb.group({
+      username: ['',[Validators.required], Validators.minLength(3)],
+      password: ['',[Validators.required, Validators.minLength(8)]],
+      nombre: ['',[Validators.required]],
+      apellido: ['',[Validators.required]],
+      email: ['',[Validators.required]],
+    })
+    
+    onSubmit() {
+      if (!this.formulario.invalid) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Registrado con exito',
+          showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+          }
+        })        
+      }
+      else{
+        Swal.fire({
+          icon: 'error',
+          title: 'Error al registrar usuario, corrija los errores marcados',
+          showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+          }
+        })
+      }
+      
+  }
+  
+  constructor(private fb: FormBuilder) { }
+  
   ngOnInit(): void {
   }
 
