d9fb270ee44bb280dd5800f837bef50f3d4b2cfa
Tomas Gonzalez <gonzalez.tomas1806@gmail.com>
2022-07-07 23:37:49 -0300

    Mejora servicio
    Mejora servicio

--- src/app/abm-alumnos/abm-alumnos.component.ts
+++ src/app/abm-alumnos/abm-alumnos.component.ts
@@ -1,7 +1,9 @@
 
 import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 import { ListadoAlumnosService } from '../servicios/listado-alumnos.service';
 import { Alumnos } from '../models/alumnos/alumnos';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-abm-alumnos',
   templateUrl: './abm-alumnos.component.html',
@@ -9,15 +11,55 @@ import { Alumnos } from '../models/alumnos/alumnos';
 })
 export class ABMAlumnosComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'nombre'];
+  columnsToDisplay: string[] = ['id', 'nombre', 'seleccionar'];
   listadoAlumnos:Alumnos[] = [];
   dataSource:any[] = [];
   constructor(private _AlumnosService: ListadoAlumnosService) {
     this.listadoAlumnos= _AlumnosService.obtenerListadoALumnos()
     this.dataSource = this.listadoAlumnos;
    }
-
   ngOnInit(): void {
   }
+  buscar(){
+    Swal.fire({
+      title: 'Ingresar tu Github username',
+      input: 'text',
+      inputAttributes: {
+        autocapitalize: 'off'
+      },
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Buscar',
+      showLoaderOnConfirm: true,
+      preConfirm: (login) => {
+        this._AlumnosService.obtenerNombreAlumno(login)
+          .then(alumno => {
+            Swal.fire({
+              title: `Se encontro a los siguientes alumnos`,
+              text: `${alumno.nombre} ${alumno.apellido}`,
+            })
+          })
+          .catch(error => {
+            Swal.fire(
+              `No se encontro al alumno buscado: ${error}`
+            )
+          })
+      },
+      allowOutsideClick: () => !Swal.isLoading()
+    })
+  }
+  seleccionar(index:number){
+    Swal.fire({
+      icon: 'success',
+      title: 'Registrado con exito',
+      showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      }
+    })
+  }
 }
+
 
--- src/app/servicios/listado-alumnos.service.ts
+++ src/app/servicios/listado-alumnos.service.ts
@@ -1,22 +1,49 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 import { Alumnos } from '../models/alumnos/alumnos';
 import { Usuario } from '../models/usuarios/usuario';
 @Injectable({
   providedIn: 'root'
 })
 export class ListadoAlumnosService {
-  
-  
-  listado:Alumnos[] = [
-    { id: 1, nombre: 'Tomas', apellido: 'Gonzalez' },
-    { id: 2, nombre: 'Maria', apellido: 'Juarez' },
-    { id: 3, nombre: 'Micaela', apellido: 'Sielas' },
-    { id: 4, nombre: 'Jorge', apellido: 'Ramirez' },
+  listado: Alumnos[] = [
+    {
+      id: 1,
+      nombre: 'Tomas',
+      apellido: 'Gonzalez'
+    },
+    {
+      id: 2,
+      nombre: 'Maria',
+      apellido: 'Juarez'
+    },
+    {
+      id: 3,
+      nombre: 'Micaela',
+      apellido: 'Sielas'
+    },
+    {
+      id: 4,
+      nombre: 'Jorge',
+      apellido: 'Ramirez'
+    },
   ];
 
   constructor() {
-   }
-   obtenerListadoALumnos(){
+  }
+  obtenerListadoALumnos() {
     return this.listado
-   }
+  }
+  obtenerNombreAlumno(nombre: string | null) {
+    return new Promise<Alumnos>((resolve, reject) => {
+      let alumno: any
+      this.listado.forEach(element => {
+        if (element.nombre == nombre) {
+          alumno = element
+          return resolve(alumno)
+        }
+      })
+      return reject({ mensaje: "No se pudo obtener el alumno buscado" })
+    });
+  }
 }
